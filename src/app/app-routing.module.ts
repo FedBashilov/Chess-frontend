9db2180af0179e1f533db8355e6e3a1f79c2a4ba
FedBashilov <>
2021-04-06 18:41:40 +0300

    Add routing path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {MessageComponent} from './components/message/message.component';
+import {GameListComponent} from './components/game-list/game-list.component';
+import {AuthorizationComponent} from './components/authorization/authorization.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'games', pathMatch: 'full'},
+  {path: 'games', component: GameListComponent},
+  {path: 'message', component: MessageComponent},
+  {path: 'auth', component: AuthorizationComponent},
+  {path: '**', redirectTo: 'games', pathMatch: 'full'}
+];;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [MessageComponent, GameListComponent]
